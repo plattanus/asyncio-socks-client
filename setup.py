cc07abe7a9a9a167a8f0965ed5f2a2e8ec47b49e
root <root@lttwksp.lttwksp.mine>
2023-12-31 02:47:37 -0800

    setup update

--- setup.py
+++ setup.py
@@ -2,32 +2,35 @@
 import os
 import re
 
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 base_path = os.path.dirname(__file__)
-
 
 with open(os.path.join(base_path, "README.md")) as f:
     long_description = f.read()
 
 with open(os.path.join(base_path, "socks_client/__version__.py")) as f:
-    try:
-        VERSION = re.findall(r"^__version__ = '([^']+)'\r?$", f.read(), re.M)[0]
-    except:
+    pattern = r'__version__ = "(.*?)"'
+    match = re.search(pattern, f.read())
+    if match:
+        VERSION = match.group(1)
+    else:
         VERSION = None
+    print(VERSION)
 
 
 setup(
     name="socks-client",
     version=VERSION,
+
     description="Supports both TCP and UDP client with the implementation of SOCKS5 and SOCKS4 protocol",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     author="Plattanus",
     author_email="plattanus@outlook.com",
     url="https://github.com/plattanus/socks-client.git",
     license="MIT License",
-    keywords=["socks", "socks5", "socks4", "proxy", "asyncio"],
+    keywords=["socks", "socks5", "socks4", "proxy", "asyncio", "tcp", "udp"],
     install_requires=[],
     python_requires=">=3.7",
     packages=[
